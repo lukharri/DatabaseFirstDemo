18b6126bf1e98bf216a9b4ba420cd05ee32e7041
Luke Harris <luke.e.harris@gmail.com>
2017-09-21 08:21:21 -0500

    initial commit

--- DatabaseFirstDemo/Program.cs
+++ DatabaseFirstDemo/Program.cs
@@ -7,9 +7,17 @@ using System.Threading.Tasks;
 namespace DatabaseFirstDemo
 {
     class Program
+    // string connString = "Server=CORI\\SQLEXPRESS; Data Source=.\\SQLEXPRESS; Initial Catalog=pubs; Integrated Security=true";
     {
         static void Main(string[] args)
         {
+
+            var dbContext = new PlutoDBContext();
+            var courses = dbContext.GetCourses();
+            foreach ( var course in courses )
+            {
+                Console.WriteLine(course.Title);
+            }
         }
     }
 }
